ac830d2f6f003ee595c6a799969519438a417ee2
James Farrell <33842132+JamesAFarrell@users.noreply.github.com>
2022-04-14 15:32:08 +0100

    only outpatient

--- analysis/04_analysis_obj_1.R
+++ analysis/04_analysis_obj_1.R
@@ -20,7 +20,7 @@ boot_ci = function(x, statistic, R){
 }
 
 bootstrap_by_group = function(data, value.var = "n", groups = NULL, 
-                              statistic = meanfun, R = 1000) {
+                              statistic = meanfun, R = 100) {
   
   data = data %>% 
     group_by(across(all_of(groups))) %>% 
@@ -41,9 +41,9 @@ dir.create(here::here("output", "descriptive", "obj_1"),
 data_patient_2019 = read_rds(here::here("output", "data", "cohorts", "data_patient_2019.rds"))
 data_patient_2020 = read_rds(here::here("output", "data", "cohorts", "data_patient_2020.rds"))
 data_patient_2021 = read_rds(here::here("output", "data", "cohorts", "data_patient_2021.rds"))
-data_admissions   = read_rds(here::here("output", "data", "data_admissions.rds"))
+#data_admissions   = read_rds(here::here("output", "data", "data_admissions.rds"))
 data_outpatient   = read_rds(here::here("output", "data", "data_outpatient.rds"))
-data_gp           = read_rds(here::here("output", "data", "data_gp.rds"))
+#data_gp           = read_rds(here::here("output", "data", "data_gp.rds"))
 
 # Extract variable labels
 vlabels = extract_variable_label(data_patient_2019)
@@ -69,8 +69,9 @@ tbl_summary_cohort_year = data_patient_2019 %>%
                      add_row_total = TRUE)
 
 # Define variables to stratify plots by ----
-strat_var = c(NULL, "sex", "age_factor", "ethnicity_comb", "region", "imd_Q5",
-              "rural_urban", "asthma", "diabetes")
+#strat_var = c(NULL, "sex", "age_factor", "ethnicity_comb", "region", "imd_Q5",
+#              "rural_urban", "asthma", "diabetes")
+strat_var = c(NULL)
 
 # Outpatient appointments ----
 strat_var %>% 
@@ -126,55 +127,55 @@ strat_var %>%
   })
 
 # GP contact ----
-strat_var %>% 
-  map(function(var){
-    gp_id = data_patient_2019 %>% 
-      select(patient_id, cohort_year, all_of(var)) %>%
-      bind_rows(
-        data_patient_2020 %>% 
-          select(patient_id, cohort_year, all_of(var))
-      ) %>% 
-      bind_rows(
-        data_patient_2021 %>% 
-          select(patient_id, cohort_year, all_of(var))
-      ) %>% 
-      group_by(across(all_of(c("patient_id", "cohort_year", var)))) %>% 
-      summarise(period_index = 1:52) %>%
-      ungroup()
-    
-    gp_count_nonzero = data_gp %>% 
-      mutate(period_index = week(gp_contact_date),
-             cohort_year = year(gp_contact_date) %>% as.character()) %>% 
-      group_by(patient_id, cohort_year, period_index) %>% 
-      count()
-    
-    gp_count = gp_id %>% 
-      left_join(gp_count_nonzero,
-                by = c("patient_id", "cohort_year", "period_index")
-      ) %>% 
-      mutate(n = replace_na(n, 0))
-    
-    gp_mean = gp_count %>% 
-      bootstrap_by_group(value.var = "n",
-                         groups = c("cohort_year", "period_index", var)) %>% 
-      mutate(date = paste0(cohort_year, "-01-01") %>% as.Date() +
-               (period_index -1)*7 +3)
-    
-    plot_gp = gp_mean %>%
-      ggplot(aes_string("date", "mean", colour = var, fill = var)) +
-      geom_line() +
-      geom_ribbon(aes_string(ymin = "ci_lower", ymax = "ci_upper"),
-                  alpha = 0.2, linetype = "dotted") +
-      theme_bw() +
-      labs(x = NULL,
-           y = "GP contacts (number per patient per week)",
-           colour = vlabels[vlabels %>% names()== var],
-           fill   = vlabels[vlabels %>% names()== var])
-    
-    ggsave(paste0("obj1_gp_", var, ".jpeg"),
-           plot = last_plot(),
-           device = "jpeg",
-           path = here::here("output", "descriptive", "obj_1"))
-  })
-
-
+# strat_var %>% 
+#   map(function(var){
+#     gp_id = data_patient_2019 %>% 
+#       select(patient_id, cohort_year, all_of(var)) %>%
+#       bind_rows(
+#         data_patient_2020 %>% 
+#           select(patient_id, cohort_year, all_of(var))
+#       ) %>% 
+#       bind_rows(
+#         data_patient_2021 %>% 
+#           select(patient_id, cohort_year, all_of(var))
+#       ) %>% 
+#       group_by(across(all_of(c("patient_id", "cohort_year", var)))) %>% 
+#       summarise(period_index = 1:52) %>%
+#       ungroup()
+#     
+#     gp_count_nonzero = data_gp %>% 
+#       mutate(period_index = week(gp_contact_date),
+#              cohort_year = year(gp_contact_date) %>% as.character()) %>% 
+#       group_by(patient_id, cohort_year, period_index) %>% 
+#       count()
+#     
+#     gp_count = gp_id %>% 
+#       left_join(gp_count_nonzero,
+#                 by = c("patient_id", "cohort_year", "period_index")
+#       ) %>% 
+#       mutate(n = replace_na(n, 0))
+#     
+#     gp_mean = gp_count %>% 
+#       bootstrap_by_group(value.var = "n",
+#                          groups = c("cohort_year", "period_index", var)) %>% 
+#       mutate(date = paste0(cohort_year, "-01-01") %>% as.Date() +
+#                (period_index -1)*7 +3)
+#     
+#     plot_gp = gp_mean %>%
+#       ggplot(aes_string("date", "mean", colour = var, fill = var)) +
+#       geom_line() +
+#       geom_ribbon(aes_string(ymin = "ci_lower", ymax = "ci_upper"),
+#                   alpha = 0.2, linetype = "dotted") +
+#       theme_bw() +
+#       labs(x = NULL,
+#            y = "GP contacts (number per patient per week)",
+#            colour = vlabels[vlabels %>% names()== var],
+#            fill   = vlabels[vlabels %>% names()== var])
+#     
+#     ggsave(paste0("obj1_gp_", var, ".jpeg"),
+#            plot = last_plot(),
+#            device = "jpeg",
+#            path = here::here("output", "descriptive", "obj_1"))
+#   })
+# 
+# 
