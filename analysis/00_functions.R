534d0078d8b8608648a57e954146371d3cecb830
James Farrell <33842132+JamesAFarrell@users.noreply.github.com>
2022-05-25 23:24:25 +0100

    update

--- analysis/00_functions.R
+++ analysis/00_functions.R
@@ -1,4 +1,14 @@
 # Functions ----
+
+fct_case_when <- function(...) {
+  # uses dplyr::case_when but converts the output to a factor,
+  # with factors ordered as they appear in the case_when's  ... argument
+  args <- as.list(match.call())
+  levels <- sapply(args[-1], function(f) f[[3]])  # extract RHS of formula
+  levels <- levels[!is.na(levels)]
+  factor(dplyr::case_when(...), levels=levels)
+}
+
 # read_column_type: read column name to determine column data type
 read_column_type = function(file){
   
--- analysis/construct_cohort.R
+++ analysis/construct_cohort.R
@@ -1,61 +1,90 @@
-# Define potential nosocomial infection ----
-# Defined as a positive covid test after day 7 in hospital and on or before 7th
-# day following discharge
-data_patient = data_patient %>%
-  left_join(
-    data_admissions %>%
-      left_join(data_patient %>% select(patient_id,
-                                        covid_positive_test_date_1),
-                by = "patient_id") %>%
-      mutate(
-        covid_nosocomial = case_when(
-          # Length of stay less than 7 days: Not nosocomial 
-          (discharge_date - admission_date) < 7 ~ NA_character_,
-          # Length of stay 7+ days: Nosocomial if positive after day 7 in hospital
-          # and on or before day 7 following discharge, otherwise not nosocomial
-          (admission_date + days(7) < covid_positive_test_date_1) &
-            (discharge_date + days(7) >= covid_positive_test_date_1) ~ "Yes",
-          TRUE ~ NA_character_
-        ) %>%
-          ff_label("Nosocomial infection")) %>%
-      filter(covid_nosocomial == "Yes") %>%
-      select(patient_id, covid_nosocomial) %>%
-      group_by(patient_id) %>%
-      slice(1) %>%
-      ungroup(),
-    by = "patient_id"
+
+# Load packages ----
+library(tidyverse)
+library(lubridate)
+library(finalfit)
+
+# Load custom functions ----
+source(here::here("analysis", "00_functions.R"))
+
+# Load datasets ----
+data_patient = readRDS(here::here("output", "data", "data_patient.rds"))
+
+
+# Cohort criteria ----
+criteria_2019 = data_patient %>% 
+  transmute(
+    patient_id = patient_id,
+    covid_status = covid_status,
+    is_alive = is.na(death_date) | death_date > ymd("2019-01-01"), 
+    isnot_nosocomial = covid_nosocomial == "No",
+    isnot_descrepant_result = covid_discrepant_test == "No",
+    is_age_4_or_more = age_2019 >= 4,
+    is_age_under_18 = age_2019 < 18,
+    include = isnot_nosocomial & isnot_descrepant_result &
+      is_age_4_or_more & is_age_under_18
   )
 
-# Discrepant test result ----
-data_patient = data_patient %>%
-  mutate(covid_discrepant_test = if_else(
-    covid_positive_test_date_1 == covid_negative_test_date_before_positive,
-    "Yes",
-    NA_character_
-  ))
+data_cohort_2019 = criteria_2019 %>%
+  filter(include) %>%
+  select(patient_id) %>%
+  left_join(data_patient, by="patient_id") %>%
+  droplevels()
 
-# Construct cohorts ----
-data_patient = data_patient %>%
+
+data_inclusioncriteria = criteria_2019 %>%
+  transmute(
+    patient_id,
+    covid_status,
+    c0 = TRUE,
+    c1 = c0 & is_alive,
+    c2 = c1 & isnot_nosocomial,
+    c3 = c2 & (isnot_descrepant_result),
+    c4 = c3 & (is_age_4_or_more & is_age_under_18),
+  )
+
+data_flowchart <-
+  data_inclusioncriteria %>%
+  select(-patient_id) %>%
+  group_by(covid_status) %>%
+  summarise(
+    across(.fns=sum)
+  ) %>%
+  pivot_longer(
+    cols=-covid_status,
+    names_to="criteria",
+    values_to="n"
+  ) %>%
+  group_by(covid_status) %>%
   mutate(
-    excl_cohort_2019 = case_when(
-      covid_nosocomial == "Yes" ~ "Probable nosocomial infection",
-      covid_discrepant_test == "Yes" ~ "Discrepant same-day RT-PCR result",
-      age_2019_factor == "Under 4" ~ "Age 4 years or below on 1st Jan 2019",
-      age_2019_factor == "18+" ~ "Age 18 years or above on 1st Jan 2019",
+    n_exclude = lag(n) - n,
+    pct_exclude = n_exclude/lag(n),
+    pct_all = n / first(n),
+    pct_step = n / lag(n),
+    crit = str_extract(criteria, "^c\\d+"),
+    criteria = fct_case_when(
+      crit == "c0" ~ "OpenSAFELY extract: Registered with GP between 1st January 2019 and 1st May 2022, and alive with age between 1 and 18 years on 01 January 2019",
+      crit == "c1" ~ "  is alive on 1st January 2019",
+      crit == "c2" ~ "  with no probable nosocomial infection",
+      crit == "c3" ~ "  with no same-day discrepent RT-PCR test result",
+      crit == "c4" ~ "  with age between 4 and 17 years",
       TRUE ~ NA_character_
-    ),
-    excl_cohort_2020 = case_when(
-      covid_nosocomial == "Yes" ~ "Probable nosocomial infection",
-      covid_discrepant_test == "Yes" ~ "Discrepant same-day RT-PCR result",
-      age_2020_factor == "Under 4" ~ "Age 4 years or below on 1st Jan 2020",
-      age_2020_factor == "18+" ~ "Age 18 years or above on 1st Jan 2020",
-      TRUE ~ NA_character_
-    ),
-    excl_cohort_2021 = case_when(
-      covid_nosocomial == "Yes" ~ "Probable nosocomial infection",
-      covid_discrepant_test == "Yes" ~ "Discrepant same-day RT-PCR result",
-      age_2021_factor == "Under 4" ~ "Age 4 years or below on 1st Jan 2021",
-      age_2021_factor == "18+" ~ "Age 18 years or above on 1st Jan 2021",
-      TRUE ~ NA_character_
-    ),
-  )
+    )
+  ) #
+
+
+
+
+data_inclusioncriteria <- criteria_2019 %>%
+  transmute(
+    patient_id,
+    vax3_type,
+    c0 = vax1_afterfirstvaxdate & vax3_afterstartdate & vax3_beforeenddate & has_expectedvax3type,
+    c1 = c0 & (has_age & has_sex & has_imd & has_ethnicity & has_region),
+    c2 = c1 & (has_vaxgap12 & has_vaxgap23 & has_knownvax1 & has_knownvax2 & vax12_homologous),
+    c3 = c2 & (isnot_hscworker ),
+    c4 = c3 & (isnot_carehomeresident & isnot_endoflife & isnot_housebound),
+    c5 = c4 & (has_norecentcovid),
+    c6 = c5 & (isnot_inhospitalunplanned),
+  ) %>%
+  filter(c0)
