67e39a2cdc7931a4b23feed79615628d0feb3a74
James Farrell <33842132+JamesAFarrell@users.noreply.github.com>
2022-08-04 16:18:35 +0100

    Update 00_utility_functions.R

--- analysis/00_utility_functions.R
+++ analysis/00_utility_functions.R
@@ -1,5 +1,207 @@
 # Functions ----
 
+
+# Applies general exclusion criteria to cohort
+apply_exclusion_criteria = function(.data_patient){
+  .data_patient  %>% 
+    filter(
+      (age >= 4) & (age < 18),
+      covid_nosocomial == "No",
+      covid_discrepant_test == "No",
+      death == "No"
+    )
+}
+
+# Bootstrap confidence intervals for incidence rate 
+boot.incidence.rate = function(counts, persontime, name = "inc_rate",
+                               R = 20, conf = 0.95, type = "perc", ...){
+  bootfunc = function(c, i, pt){
+    n = sum(c[i])
+    d = sum(pt[i])
+    return(n/d)
+  }
+  
+  boot_ci = boot(counts, bootfunc, R = R, pt = persontime, ...) %>% 
+    boot.ci(conf = conf, type = type)
+  boot_result = c(boot_ci$t0, boot_ci$percent[4:5])
+  names(boot_result) = paste0(name, c("_est", "_LL", "_UL"))
+  return(boot_result)
+}
+
+# Calculates age and age group at index date
+calc_age = function(.data_patient, index_date){
+  .data_patient %>% 
+    mutate(
+      age = time_length(
+        interval(date_of_birth, index_date),
+        unit = "years"
+      ) %>% 
+        ff_label("Age (years)"),
+      age_group = age %>%
+        cut(
+          breaks = c(-Inf, 4, 7, 11, 15, 18, Inf),
+          labels = c("Under 4", "4-6", "7-10", "11-14", "15-17", "18+")
+        ) %>% 
+        ff_label("Age group (years)")
+    )
+}
+
+# Calculates comorbidity status given index date
+calc_comorbidity_status = function(.data_patient, index_date){
+  .data_patient %>% 
+    mutate(
+      asthma = case_when(
+        asthma_first_date < index_date ~ "Yes",
+        is.na(asthma_first_date) ~ "No",
+        TRUE ~ "No"
+      ) %>%
+        factor() %>%
+        ff_label("Asthma"),
+      
+      cancer = case_when(
+        cancer_first_date < index_date ~ "Yes",
+        is.na(cancer_first_date) ~ "No",
+        TRUE ~ "No"
+      ) %>%
+        factor() %>%
+        ff_label("Cancer"),
+      
+      diabetes = case_when(
+        diabetes_first_date < index_date ~ "Yes",
+        is.na(diabetes_first_date) ~ "No",
+        TRUE ~ "No"
+      ) %>%
+        factor() %>%
+        ff_label("Diabetes"),
+      
+      epilepsy = case_when(
+        epilepsy_first_date < index_date ~ "Yes",
+        is.na(epilepsy_first_date) ~ "No",
+        TRUE ~ "No"
+      ) %>%
+        factor() %>%
+        ff_label("Epilepsy"),
+      
+      severe_mental_illness = case_when(
+        severe_mental_illness_first_date < index_date ~ "Yes",
+        is.na(severe_mental_illness_first_date) ~ "No",
+        TRUE ~ "No"
+      ) %>%
+        factor() %>%
+        ff_label("Severe mental illness"),
+      
+      cerebral_palsy = case_when(
+        cerebral_palsy_first_date < index_date ~ "Yes",
+        is.na(cerebral_palsy_first_date) ~ "No",
+        TRUE ~ "No"
+      ) %>%
+        factor() %>%
+        ff_label("Cerebral palsy"),
+      
+      chronic_infections = case_when(
+        chronic_infections_first_date < index_date ~ "Yes",
+        is.na(chronic_infections_first_date) ~ "No",
+        TRUE ~ "No"
+      ) %>%
+        factor() %>%
+        ff_label("Chronic infections"),
+      
+      devices_and_stomas = case_when(
+        devices_and_stomas_first_date < index_date ~ "Yes",
+        is.na(devices_and_stomas_first_date) ~ "No",
+        TRUE ~ "No"
+      ) %>%
+        factor() %>%
+        ff_label("Devices and stomas"),
+      
+      endocrine_disorders = case_when(
+        endocrine_disorders_first_date < index_date ~ "Yes",
+        is.na(endocrine_disorders_first_date) ~ "No",
+        TRUE ~ "No"
+      ) %>%
+        factor() %>%
+        ff_label("Endocrine disorders"),
+      
+      gastrointestinal_disorders = case_when(
+        gastrointestinal_disorders_first_date < index_date ~ "Yes",
+        is.na(gastrointestinal_disorders_first_date) ~ "No",
+        TRUE ~ "No"
+      ) %>%
+        factor() %>%
+        ff_label("Gastrointestinal disorders"),
+      
+      haematological_disorders = case_when(
+        haematological_disorders_first_date < index_date ~ "Yes",
+        is.na(haematological_disorders_first_date) ~ "No",
+        TRUE ~ "No"
+      ) %>%
+        factor() %>%
+        ff_label("Haematological disorders"),
+      
+      immunological_disorders = case_when(
+        immunological_disorders_first_date < index_date ~ "Yes",
+        is.na(immunological_disorders_first_date) ~ "No",
+        TRUE ~ "No"
+      ) %>%
+        factor() %>%
+        ff_label("Immunological disorders"),
+      
+      learning_and_behaviour_difficulties = case_when(
+        learning_and_behaviour_difficulties_first_date < index_date ~ "Yes",
+        is.na(learning_and_behaviour_difficulties_first_date) ~ "No",
+        TRUE ~ "No"
+      ) %>%
+        factor() %>%
+        ff_label("Learning and behavioural difficulties"),
+      
+      mental_illness = case_when(
+        mental_illness_first_date < index_date ~ "Yes",
+        is.na(mental_illness_first_date) ~ "No",
+        TRUE ~ "No"
+      ) %>%
+        factor() %>%
+        ff_label("Mental illness"),
+      
+      musculoskeletal_and_rheum = case_when(
+        musculoskeletal_and_rheum_first_date < index_date ~ "Yes",
+        is.na(musculoskeletal_and_rheum_first_date) ~ "No",
+        TRUE ~ "No"
+      ) %>%
+        factor() %>%
+        ff_label("Musculoskeletal and rheumatic diseases"),
+      
+      transplant = case_when(
+        transplant_first_date < index_date ~ "Yes",
+        is.na(transplant_first_date) ~ "No",
+        TRUE ~ "No"
+      ) %>%
+        factor() %>%
+        ff_label("Transplant")
+    ) 
+}
+
+# Calculate death status at index date
+calc_death_status = function(.data_patient, index_date){
+  .data_patient %>% 
+    mutate(
+      death = case_when(
+        death_date < index_date ~ "Yes",
+        is.na(death_date) ~ "No",
+        TRUE ~ "No"
+      ) %>%
+        factor() %>%
+        ff_label("Death")
+    )
+}
+
+# Wrapper function for calculating time-dependent variables
+calc_indexed_variables = function(.data_patient, index_date){
+  .data_patient %>% 
+    calc_age(index_date) %>%
+    calc_comorbidity_status(index_date) %>%
+    calc_death_status(index_date)
+}
+
 fct_case_when <- function(...) {
   # uses dplyr::case_when but converts the output to a factor,
   # with factors ordered as they appear in the case_when's  ... argument
@@ -58,6 +260,14 @@ read_column_type = function(file){
     paste(collapse = "")
   
   return(type_string)
+}
+
+# Returns vector of mean and confidence intervals from t.test()
+mean.cl.ttest = function(x){
+  t_test = t.test(x)
+  x_ci = c(t_test$estimate, t_test$conf.int[1], t_test$conf.int[2])
+  names(x_ci) = c("Mean", "Lower", "Upper")
+  x_ci
 }
 
 # plot_hist: plot histogram given data and variable name
