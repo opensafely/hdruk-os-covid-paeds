5ad6908b1752790d49e89f221f41f66c508fe812
James Farrell <33842132+JamesAFarrell@users.noreply.github.com>
2022-08-15 00:35:45 +0100

    Update 05_healthcare_use_2019_2022.R

--- analysis/05_healthcare_use_2019_2022.R
+++ analysis/05_healthcare_use_2019_2022.R
@@ -25,21 +25,21 @@ global_var = jsonlite::read_json(path = here::here("analysis", "global_variables
 # Disclosure control parameters ----
 count_round = global_var$disclosure_count_round
 
-## Study dates ----
+# Study dates ----
 study_start_date = ymd(global_var$start_date)
 study_end_date   = ymd(global_var$end_date)
 tp_start_date    = ymd(global_var$tp_start_date)
 tp_end_date      = ymd(global_var$tp_end_date)
 fup_start_date   = ymd(global_var$fup_start_date)
 
-# Load datasets
-data_patient = read_rds(here::here("output", "data", "data_patient.rds"))
-data_testing = read_rds(here::here("output", "data", "data_testing.rds"))
+# Load datasets ----
+data_patient    = read_rds(here::here("output", "data", "data_patient.rds"))
 data_admissions = read_rds(here::here("output", "data", "data_admissions.rds"))
 data_outpatient = read_rds(here::here("output", "data", "data_outpatient.rds"))
-data_gp = read_rds(here::here("output", "data", "data_gp.rds"))
+data_gp         = read_rds(here::here("output", "data", "data_gp.rds"))
 
-# Cohort construction ----
+# Yearly cohort construction ----
+## Index dates ----
 index_date = c("2019-01-01",
                "2020-01-01",
                "2021-01-01",
@@ -47,91 +47,56 @@ index_date = c("2019-01-01",
   ymd() %>% 
   as.list()
 
-data_criteria = map(.x = index_date,
-                    .data_patient = data_patient,
-                    .f = function(.index_date, .data_patient){
-                      data_criteria = .data_patient %>% 
-                        transmute(
-                          patient_id,
-                          index_date = .index_date,
-                          cohort = year(index_date),
-                          age_indexed = time_length(
-                            interval(date_of_birth, .index_date),
-                            unit = "years"
-                          ) %>% 
-                            ff_label("Age on 1st January (years)"),
-                          age_group_indexed = age_indexed %>%
-                            cut(
-                              breaks = c(-Inf, 4, 7, 11, 15, 18, Inf),
-                              labels = c("Under 4", "4-6", "7-10", "11-14", "15-17", "18+")
-                            )%>%
-                            ff_label("Age group on 1st January (years)"),
-                          is_alive = is.na(death_date) | death_date > index_date, 
-                          isnot_nosocomial = covid_nosocomial == "No",
-                          isnot_descrepant_result = covid_discrepant_test == "No",
-                          is_age_4_or_more = age_indexed >= 4,
-                          is_age_under_18 = age_indexed < 18,
-                          include = is_alive & 
-                            isnot_nosocomial & 
-                            isnot_descrepant_result &
-                            is_age_4_or_more & is_age_under_18
-                        )
-                    }
-)
-
-data_cohort = data_criteria %>% 
-  map(
-    function(.data_criteria){
-      data_cohort = .data_criteria %>%
-        filter(include) %>%
-        select(patient_id, cohort, index_date, age_indexed, age_group_indexed) %>% 
-        left_join(
-          data_patient, by = "patient_id"
-        )
-    }
+## Create cohort data for each year ----
+data_cohort = index_date %>% 
+  map(function(.index_date, .data_patient){
+    data_cohort = .data_patient %>%
+      mutate(cohort = year(.index_date)) %>% 
+      calc_indexed_variables(.index_date)
+  },
+  .data_patient = data_patient
   )
 
-
 # Inclusion flowchart ----
-flowchart = data_criteria %>% 
-  map(
-    function(.data_criteria){
-      ## Create inclusion flowchart ----
-      flowchart = .data_criteria %>%
+## Calculate inclusion flowchart ----
+tbl_flowchart = data_cohort %>% 
+  map2(
+    index_date,
+    function(.data_cohort, .index_date){
+      data_criteria = .data_cohort %>% 
+        count_exclusion_criteria() %>% 
         transmute(
-          patient_id,
-          cohort,
           c0 = TRUE,
-          c1 = c0 & isnot_nosocomial,
-          c2 = c1 & isnot_descrepant_result,
+          c1 = c0 & is_not_covid_nosocomial,
+          c2 = c1 & is_not_covid_discrepant_test,
           c3 = c2 & is_alive,
-          c4 = c3 & (is_age_4_or_more & is_age_under_18),
+          c4 = c3 & is_aged_between_4_17,
         ) %>%
-        select(-patient_id) %>%
-        group_by(cohort) %>%
         summarise(
           across(.fns=sum)
         ) %>%
+        mutate(pivot_col = NA) %>% 
         pivot_longer(
-          cols=-cohort,
+          cols=-pivot_col,
           names_to="criteria",
           values_to="n"
-        ) %>%
-        group_by(cohort) %>%
+        ) %>% 
+        select(-pivot_col) %>% 
         mutate(
           n = n %>% plyr::round_any(count_round)
         ) %>% 
         mutate(
+          cohort = year(.index_date),
           n_exclude = lag(n) - n,
           pct_all = (n/first(n)) %>% percent(0.1),
           pct_exclude_step = (n_exclude/lag(n)) %>% percent(0.1),
           crit = str_extract(criteria, "^c\\d+"),
           criteria = fct_case_when(
             crit == "c0" ~ "OpenSAFELY extract: Registered with GP, alive, with age >0 and <18 years on 01 January 2019",
-            crit == "c1" ~ "-  with no probable nosocomial infection",
-            crit == "c2" ~ "-  with no same-day discrepent RT-PCR test result",
-            crit == "c3" ~ "-  is alive on 1st January",
-            crit == "c4" ~ "-  with age between 4 and 17 years inclusive",
+            crit == "c1" ~ "  with no probable nosocomial infection",
+            crit == "c2" ~ "  with no same-day discrepent RT-PCR test result",
+            crit == "c3" ~ "  is alive on 1st January",
+            crit == "c4" ~ "  with age between 4 and 17 years inclusive",
             TRUE ~ NA_character_
           )
         ) %>%
@@ -139,89 +104,79 @@ flowchart = data_criteria %>%
         replace_na(
           list(n_exclude = "-", pct_exclude_step = "-")
         )
-    }
-  ) %>% 
-  bind_rows()
-
-tbl_flowchart = flowchart %>% 
+    }) %>% 
+  bind_rows() %>% 
   select(cohort, criteria, n, n_exclude, pct_all, pct_exclude_step) %>%
   group_by(cohort) %>% 
   mutate(cohort = if_else(row_number() == 1, cohort %>% as.character(), ""))
 
+## Save inclusion flowchart ----
 write_csv(tbl_flowchart, 
           here::here("output", "descriptives", "healthcare_use_2019_2022",
                      "tbl_flowchart.csv"))
 
+# Filter cohort based on exclusion criteria ----
+data_cohort = data_cohort %>% 
+  map(function(.data_cohort){
+    .data_cohort %>%
+      apply_exclusion_criteria()
+  })
 
-# Cohort summary ----
-
+# Cohort summary table ----
 explanatory_var = c(
-  "age_indexed",
-  "age_group_indexed",
-  "sex",
-  "ethnicity",
-  "imd_Q5_2019",
-  "region_2019",
+  # Covid status
+  "covid_status_tp",
+  
+  # Demographics
+  "age", "age_group", "sex", "ethnicity", "imd_Q5_2019", "region_2019",
   "rural_urban_2019",
   
-  "covid_status_tp",
-  "covid_status_fup",
-  "covid_test_neg_tp_count",
-  "covid_test_pos_tp_count",
-  "covid_test_neg_fup_count",
-  "covid_test_pos_fup_count",
+  # Shielding
+  #"shielding",
   
-  "asthma",
-  "cancer",
-  "diabetes",
-  "epilepsy",
-  "severe_mental_illness",
-  
-  "cerebral_palsy",
-  "chronic_infections",
-  "devices_and_stomas",
-  "endocrine_disorders",
-  "gastrointestinal_disorders",
-  "haematological_disorders",
-  "immunological_disorders", 
-  "learning_and_behaviour_difficulties", 
-  "mental_illness",
-  "musculoskeletal_and_rheum",
-  "transplant"
+  # Comorbidities
+  "comorbidity_count.factor",
+  "asthma", "cancer", "cerebral_palsy", "chronic_infections",
+  "devices_and_stomas", "diabetes", "endocrine_disorders",
+  "epilepsy", "gastrointestinal_disorders", "haematological_disorders",
+  "immunological_disorders", "learning_and_behaviour_difficulties",
+  "mental_illness", "musculoskeletal_and_rheum",
+  "severe_mental_illness", "transplant"
+
 )
 
+## Create cohort summary table ----
 tbl_cohort_summary = data_cohort %>% 
-  map(
-    .explanatory_var = explanatory_var,
-    .f = function(.data_cohort, .explanatory_var){
-      .data_cohort %>%
-        mutate(cohort = cohort %>% factor()) %>% 
-        summary_factorlist(
-          dependent = "cohort",
-          explanatory = .explanatory_var,
-          total_col = FALSE,
-          add_col_totals = TRUE,
-          na_include = TRUE
-          
-        ) %>% 
-        mutate(row_num = row_number())
-    }) %>% 
+  map(function(.data_cohort){
+    .data_cohort %>%
+      mutate(cohort = cohort %>% factor()) %>% 
+      summary_factorlist(
+        dependent = "cohort",
+        explanatory = explanatory_var,
+        cont = "median",
+        total_col = FALSE,
+        add_col_totals = TRUE,
+        na_include = TRUE
+        
+      ) %>% 
+      mutate(row_num = row_number())
+  }) %>% 
   reduce(left_join, by = c("row_num", "label", "levels")) %>% 
   select(-row_num) %>% 
-  ff_round_counts(count_round) 
+  ff_round_counts(count_round)
 
+## Save cohort summary table ----
 write_csv(tbl_cohort_summary, 
           here::here("output", "descriptives", "healthcare_use_2019_2022",
                      "tbl_cohort_summary.csv"))
 
-
 # Calculate resource use ----
 time_seq = seq(study_start_date, study_end_date - months(1), by = "month")
-group_variable_list = c("overall", "asthma")
+group_variable_list = c("overall", "covid_status_tp", "age_group")
 
 ## Bed-days ----
-### Create bed-days dataset ----
-admission_counts = data_admissions %>%
+### Count bed-days by month ----
+counts_bed_days = data_admissions %>%
   rowwise() %>% 
   mutate(dates = list(seq(admission_date, discharge_date, by = "day"))) %>% 
   unnest(dates) %>% 
@@ -238,66 +193,219 @@ admission_counts = data_admissions %>%
   summarise(
     bed_days = sum(daily_count)
   ) %>% 
-  ungroup()
-           
-### Calculate bed-day incidence rate ----
-bedday_rate = time_seq %>% 
-  map(
-    function(index_date, .data_patient, .data_bed_days,
-             group_variable = "overall"){
-      
-      .data_patient = .data_patient %>% 
-        calc_indexed_variables(index_date) %>% 
-        apply_exclusion_criteria() %>% 
-        mutate(
-          overall = "Overall"
-        )
-      
-      map(
-        group_variable,
-        function(group_variable, index_date,
-                 .data_patient, .data_bed_days){
-          mean_bed_days = .data_patient %>% 
-            select(patient_id, death_date, group = all_of(group_variable)) %>%
-            mutate(
-              date_period = index_date,
-              patient_years = (pmin(death_date, index_date + months(1), na.rm = TRUE) - 
-                                 pmin(death_date, index_date, na.rm = TRUE)) %>%
-                as.numeric() / 365.25
-            ) %>% 
-            left_join(
-              admission_counts,
-              by = c("patient_id", "date_period")
-            ) %>% 
-            replace_na(list(bed_days = 0)) %>% 
-            group_by(group) %>%
-            summarise(
-              index_date,
-              group_variable,
-              n_patient = n(),
-              bed_days_total = round(sum(bed_days)),
-              patient_years_total = sum(patient_years),
-              bedday_rate = bed_days_total/patient_years_total,
-              bedday_LL = poisson.test(bed_days_total, patient_years_total)$conf.int[1],
-              bedday_UL = poisson.test(bed_days_total, patient_years_total)$conf.int[2]
-            )
-        },
-        .data_patient = .data_patient,
-        .data_bed_days = .data_bed_days,
-        index_date = index_date
-      ) %>% 
-        bind_rows()
-    },
-    .data_patient = data_patient,
-    .data_bed_days = data_bed_days,
-    group_variable = group_variable_list
-  ) %>% 
-  bind_rows()
+  ungroup() %>% 
+  mutate(cohort = year(date_period))
 
-### Format table ----
-bedday_rate %>% 
-  relocate(group_variable, index_date, group) %>% 
-  mutate(group_variable = group_variable %>%
-           factor(levels = group_variable_list)) %>% 
-  arrange(group_variable, index_date)
+### Create monthly bed-days table ----
+tbl_monthly_bed_days = group_variable_list %>% 
+  map(function(.group_var){
+    
+    data_cohort %>% 
+      map2(index_date, function(.data_cohort, .index_date, .group_var){
+    
+        .data_cohort = .data_cohort %>% 
+          mutate(overall = "Overall")
+        
+        numerator = counts_bed_days %>% 
+          filter(cohort == year(.index_date),
+                 date_period %in% time_seq,
+                 patient_id %in% .data_cohort$patient_id) %>% 
+          left_join(
+            .data_cohort %>% 
+              select(patient_id, group = all_of(.group_var)),
+            by = c("patient_id")
+          ) %>% 
+          group_by(cohort, date_period, group) %>% 
+          summarise(
+            group_var = .group_var,
+            bed_days = sum(bed_days)
+          )
+        
+        denominator = .data_cohort %>%
+          select(patient_id, cohort, group = all_of(.group_var)) %>% 
+          group_by(cohort, group) %>% 
+          summarise(
+            n_patient = n()
+          )
+        
+        numerator %>% 
+          left_join(denominator,
+                    by = c("cohort", "group"))
+      },
+      .group_var = .group_var)
+  }) %>% 
+  bind_rows() %>% 
+  relocate(cohort, date_period, group_var, group) %>% 
+  mutate(mean_beddays = bed_days/n_patient)
 
+### Save table ----
+write_csv(tbl_monthly_bed_days, 
+          here::here("output", "descriptives", "healthcare_use_2019_2022",
+                     "tbl_monthly_bed_days.csv"))
+
+## Admissions ----
+### Count admissions by month ----
+counts_admissions = data_admissions %>%
+  mutate(date_period = floor_date(admission_date, unit = "month")) %>%
+  group_by(patient_id, date_period) %>% 
+  count(date_period) %>% 
+  mutate(cohort = year(date_period))
+
+### Create monthly bed-days table ----
+tbl_monthly_admissions = group_variable_list %>% 
+  map(function(.group_var){
+    
+    data_cohort %>% 
+      map2(index_date, function(.data_cohort, .index_date, .group_var){
+        
+        .data_cohort = .data_cohort %>% 
+          mutate(overall = "Overall")
+        
+        numerator = counts_admissions %>% 
+          filter(cohort == year(.index_date),
+                 date_period %in% time_seq,
+                 patient_id %in% .data_cohort$patient_id) %>% 
+          left_join(
+            .data_cohort %>% 
+              select(patient_id, group = all_of(.group_var)),
+            by = c("patient_id")
+          ) %>% 
+          group_by(cohort, date_period, group) %>% 
+          summarise(
+            group_var = .group_var,
+            admissions = sum(n)
+          )
+        
+        denominator = .data_cohort %>%
+          select(patient_id, cohort, group = all_of(.group_var)) %>% 
+          group_by(cohort, group) %>% 
+          summarise(
+            n_patient = n()
+          )
+        
+        numerator %>% 
+          left_join(denominator,
+                    by = c("cohort", "group"))
+      },
+      .group_var = .group_var)
+  }) %>% 
+  bind_rows() %>% 
+  relocate(cohort, date_period, group_var, group) %>% 
+  mutate(mean_admissions = admissions/n_patient)
+
+### Save table ----
+write_csv(tbl_monthly_admissions, 
+          here::here("output", "descriptives", "healthcare_use_2019_2022",
+                     "tbl_monthly_admissions.csv"))
+
+## Outpatient appointments ----
+### Count outpatient appointments by month ----
+counts_outpatient = data_outpatient %>%
+  mutate(date_period = floor_date(outpatient_date, unit = "month")) %>%
+  group_by(patient_id, date_period) %>% 
+  summarise(outpatient_appts = sum(outpatient_count)) %>% 
+  mutate(cohort = year(date_period))
+
+### Create monthly outpatient table ----
+tbl_monthly_outpatient = group_variable_list %>% 
+  map(function(.group_var){
+    
+    data_cohort %>% 
+      map2(index_date, function(.data_cohort, .index_date, .group_var){
+        
+        .data_cohort = .data_cohort %>% 
+          mutate(overall = "Overall")
+        
+        numerator = counts_outpatient %>% 
+          filter(cohort == year(.index_date),
+                 date_period %in% time_seq,
+                 patient_id %in% .data_cohort$patient_id) %>% 
+          left_join(
+            .data_cohort %>% 
+              select(patient_id, group = all_of(.group_var)),
+            by = c("patient_id")
+          ) %>% 
+          group_by(cohort, date_period, group) %>% 
+          summarise(
+            group_var = .group_var,
+            outpatient_appts = sum(outpatient_appts)
+          )
+        
+        denominator = .data_cohort %>%
+          select(patient_id, cohort, group = all_of(.group_var)) %>% 
+          group_by(cohort, group) %>% 
+          summarise(
+            n_patient = n()
+          )
+        
+        numerator %>% 
+          left_join(denominator,
+                    by = c("cohort", "group"))
+      },
+      .group_var = .group_var)
+  }) %>% 
+  bind_rows() %>% 
+  relocate(cohort, date_period, group_var, group) %>% 
+  mutate(mean_outpatient = outpatient_appts/n_patient)
+
+### Save table ----
+write_csv(tbl_monthly_outpatient, 
+          here::here("output", "descriptives", "healthcare_use_2019_2022",
+                     "tbl_monthly_outpatient.csv"))
+
+## GP data health episodes ----
+### Count GP health episodes by month ----
+counts_gp = data_gp %>%
+  filter(snomed_tag %in% c("disorder", "finding", "observable_entity",
+                           "procedure", "regime_therapy", "specimen")) %>% 
+  distinct(patient_id, gp_date) %>% 
+  mutate(date_period = floor_date(gp_date, unit = "month")) %>% 
+  count(patient_id, date_period) %>% 
+  mutate(cohort = year(date_period))
+
+### Create monthly GP table ----
+tbl_monthly_gp = group_variable_list %>% 
+  map(function(.group_var){
+    
+    data_cohort %>% 
+      map2(index_date, function(.data_cohort, .index_date, .group_var){
+        
+        .data_cohort = .data_cohort %>% 
+          mutate(overall = "Overall")
+        
+        numerator = counts_gp %>% 
+          filter(cohort == year(.index_date),
+                 date_period %in% time_seq,
+                 patient_id %in% .data_cohort$patient_id) %>% 
+          left_join(
+            .data_cohort %>% 
+              select(patient_id, group = all_of(.group_var)),
+            by = c("patient_id")
+          ) %>% 
+          group_by(cohort, date_period, group) %>% 
+          summarise(
+            group_var = .group_var,
+            gp_contact = sum(n)
+          )
+        
+        denominator = .data_cohort %>%
+          select(patient_id, cohort, group = all_of(.group_var)) %>% 
+          group_by(cohort, group) %>% 
+          summarise(
+            n_patient = n()
+          )
+        
+        numerator %>% 
+          left_join(denominator,
+                    by = c("cohort", "group"))
+      },
+      .group_var = .group_var)
+  }) %>% 
+  bind_rows() %>% 
+  relocate(cohort, date_period, group_var, group) %>% 
+  mutate(mean_gp = gp_contact/n_patient)
+
+### Save table ----
+write_csv(tbl_monthly_gp, 
+          here::here("output", "descriptives", "healthcare_use_2019_2022",
+                     "tbl_monthly_gp.csv"))
