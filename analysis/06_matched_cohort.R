2ced089bd5ce8e90ab9f474b611764a6c500dd02
James Farrell <33842132+JamesAFarrell@users.noreply.github.com>
2022-08-16 00:33:54 +0100

    Update 06_matched_cohort.R

--- analysis/06_matched_cohort.R
+++ analysis/06_matched_cohort.R
@@ -1,4 +1,4 @@
-# ISARIC WHO CCP-UK study: 4C Mortality Score
+# Studying the Long-term Impact of COVID-19 in Kids (SLICK)
 # Create matched cohort
 # 06_matched_cohort.R
 # Centre for Medical Informatics, Usher Institute, University of Edinburgh 2022
@@ -10,8 +10,7 @@
 # potential matches once matched. Untested patients are randomly assigned
 # a test date based on the distribution of matched test dates and matched
 # with a positive patient. The final matched dataset consists of sets of
-# matched patients with a positive:negative:untested ratio of 1:m:m
-# (m = 10).
+# matched patients with a positive:negative:untested ratio of 1:5:5.
 
 # Load packages ----
 library(tidyverse)
@@ -45,11 +44,12 @@ fup_start_date   = ymd(global_var$fup_start_date)
 
 # Matching parameters ----
 match_ratio = 5
-test_period_span = "1 week"
+match_window = 1 
 
 # Load datasets ----
-data_patient = read_rds(here::here("output", "data", "data_patient.rds"))
-data_testing = read_rds(here::here("output", "data", "data_testing.rds"))
+data_patient    = read_rds(here::here("output", "data", "data_patient.rds"))
+data_testing    = read_rds(here::here("output", "data", "data_testing.rds"))
+data_admissions = read_rds(here::here("output", "data", "data_admissions.rds"))
 
 # Create dataset to log inclusion for inclusion flowchart ----
 data_inclusion = data_patient %>% 
@@ -65,7 +65,7 @@ data_testing_tp = data_testing %>%
   filter(test_date >= tp_start_date,
          test_date < tp_end_date)
 
-# Filter out nosocomial covid and descrepant covid test results ----
+# Filter out nosocomial covid and discrepant covid test results ----
 data_patient = data_patient %>% 
   filter(covid_nosocomial == "No", covid_discrepant_test == "No")
 
@@ -103,70 +103,106 @@ data_inclusion = data_inclusion %>%
               select(patient_id, age_criteria_test_date),
             by = c("patient_id"))
 
-## Make results factor, remove columns ----
+## Filter out in-hospital test-dates for negative patients ----
 data_testing_tp = data_testing_tp %>% 
-  mutate(result = result %>%
-           factor() %>% 
-           fct_relevel("Negative", "Positive"),
-         test_period = floor_date(test_date, test_period_span)) %>% 
-  select(patient_id, result, covid_status_tp, test_date, test_period)
+  left_join(
+    data_testing_tp %>%
+      filter(covid_status_tp == "Negative") %>% 
+      left_join(
+        data_admissions %>% 
+          select(patient_id, admission_date, discharge_date),
+        by = "patient_id"
+      ) %>% 
+      filter((test_date >= admission_date) &
+               (test_date <= discharge_date)) %>% 
+      mutate(in_hospital_neg_test = TRUE) %>%
+      distinct(patient_id, test_date, result, in_hospital_neg_test),
+    by = c("patient_id", "test_date", "result")
+  ) %>% 
+  filter(is.na(in_hospital_neg_test))
+
+### Log number of patients not hospitaled during negative test ----
+data_inclusion = data_inclusion %>% 
+  left_join(data_testing_tp %>%
+              group_by(patient_id) %>% 
+              slice(1) %>% 
+              ungroup() %>% 
+              mutate(not_in_hospital_neg_test = TRUE) %>% 
+              select(patient_id, not_in_hospital_neg_test),
+            by = c("patient_id"))
+
+# Remove unnecessary columns ----
+data_testing_tp = data_testing_tp %>% 
+  select(patient_id, result, test_date, covid_status_tp)
+
+# Split testing data into positives and negatives ----
+data_testing_pos = data_testing_tp %>% 
+  filter(covid_status_tp == "Positive")
+
+data_testing_neg = data_testing_tp %>% 
+  filter(covid_status_tp == "Negative")
 
 # Match positives with negatives ----
 ## Matching set up ----
 matched = vector()
 
-match_pos_neg = data_testing_tp %>% 
-  group_by(test_period) %>% 
-  group_map(
-    function(.x, .y){
+match_pos_neg = data_testing_pos %>%
+  group_by(test_date) %>% 
+  group_map(function(.x, .y){
+    
+    # Positive test date ----
+    pos_test_date = .y %>%
+      pull(test_date) %>% 
+      ymd()
+    
+    # Positive pool ----
+    df_pool_pos = .x
+    
+    # Negative pool ----
+    # Restrict to match window, remove matched negatives from last iteration ----
+    df_pool_neg = data_testing_neg %>%
+      filter(test_date < pos_test_date + days(match_window),
+             test_date > pos_test_date - days(match_window)) %>% 
+      filter(!patient_id %in% matched) %>%
+      group_by(patient_id) %>%
+      slice(1) %>%
+      ungroup()
+    
+    # Calculate number of potential matches in each pool ----
+    n_pos = df_pool_pos %>% nrow()
+    n_neg = df_pool_neg %>% nrow()
+    n_match = min(n_pos, floor(n_neg/match_ratio))
+    
+    # If potential matches exist, otherwise skip ----
+    if(n_match > 0){
       
-      # Remove matched controls from last iteration
-      df_in = .x %>%
-        filter(!patient_id %in% matched) %>% 
-        group_by(patient_id) %>% 
-        slice(1) %>% 
-        ungroup()
+      # Filter pools based on maximum possible matches ----
+      df_pool_pos = df_pool_pos %>% 
+        slice(1:n_match) %>% 
+        mutate(match_id = paste0(.y, 1:n_match))
       
-      # Count number of tests ----
-      n_positive = sum(df_in$result == "Positive")
-      n_negative = sum(df_in$result == "Negative")
+      df_pool_neg = df_pool_neg %>% 
+        slice(1:(n_match*match_ratio)) %>% 
+        mutate(match_id = paste0(.y, rep(1:n_match, each = match_ratio)))
       
-      # If insufficient numbers, skip matching.
-      if(n_positive == 0 | n_negative < match_ratio){
-        df_out = NULL
-      } else{
-        
-        # Reduce positives if insufficient negative potential matches ----
-        if(n_negative < n_positive*match_ratio){
-          df_in = df_in %>%
-            group_by(result) %>% 
-            filter(!(result == "Positive" & 
-                       (row_number() > floor(n_negative/match_ratio)))) %>% 
-            ungroup()
-        }
-        
-        # Match
-        df_out = matchit(result ~ test_period,
-                         data = df_in,
-                         exact = ~ test_period,
-                         ratio = match_ratio,
-                         replace = FALSE)
-        
-        # Unique ID for subclasses (date pasted with subclass)
-        df_out = match.data(df_out) %>% 
-          mutate(subclass = paste0(.y, subclass))
-        
-        # Update remove vector
-        matched_iter = df_out %>%
-          pull(patient_id)
-        matched <<- c(matched, matched_iter)
-      }
-      return(df_out)
-    },
-    .keep = TRUE
+      # Bind pools ----
+      df_out = df_pool_pos %>% 
+        bind_rows(df_pool_neg)
+      
+      # Update remove vector
+      matched_iter = df_out %>%
+        pull(patient_id)
+      matched <<- c(matched, matched_iter)
+      
+    } else{
+      df_out = NULL
+    }
+    
+    return(df_out)
+  },
+  .keep = TRUE
   ) %>% 
-  map_df(bind_rows) %>% 
-  rename(match_id = subclass)
+  map_df(bind_rows)
 
 # Match positive cases with untested ----
 ## Create dataset of untested ----
@@ -179,9 +215,8 @@ data_untested = data_patient %>%
         filter(result == "Positive") %>%
         pull(test_date),
       size = n(),
-      replace = TRUE),
-    test_period = floor_date(test_date, test_period_span)
-    )
+      replace = TRUE)
+  )
 
 ## Filter out dead patients on matched test date ----
 data_untested = data_untested %>% 
@@ -200,7 +235,7 @@ data_untested = data_untested %>%
   mutate(
     age_test_date = time_length(difftime(test_date, date_of_birth), "years"),
     result = "Untested"
-    ) %>% 
+  ) %>% 
   filter(age_test_date >= 4, age_test_date < 18)
 
 ### Log number of patients satisfying age criteria ----
@@ -210,51 +245,68 @@ data_inclusion = data_inclusion %>%
               select(patient_id, age_criteria_matched_date),
             by = "patient_id")
 
-## Bind dataset of untested with positive ----
-data_untested = data_untested %>% 
-  bind_rows(
-    match_pos_neg %>% 
-      filter(result == "Positive") %>% 
-      select(patient_id, result, covid_status_tp, test_date, test_period, match_id)
-  ) %>% 
-  mutate(
-    result = result %>% 
-      factor() %>% 
-      fct_relevel("Untested", "Positive")
-  ) 
-
+## Filter out in-hospital test-dates for negative patients ----
 data_untested = data_untested %>% 
   left_join(
-    data_untested %>% 
-      count(result, test_period) %>% 
-      pivot_wider(names_from = "result",
-                  values_from = "n",
-                  names_prefix = "n_"),
-    by = "test_period"
-  ) %>%
-  group_by(result, test_period) %>% 
-  filter(
-    ((result == "Positive") & (row_number() <= n_Untested/match_ratio)) |
-      ((result == "Untested") & (row_number() <= n_Positive*match_ratio))
+    data_untested %>%
+      left_join(
+        data_admissions %>% 
+          select(patient_id, admission_date, discharge_date),
+        by = "patient_id"
+      ) %>% 
+      filter((test_date >= admission_date) &
+               (test_date <= discharge_date)) %>% 
+      mutate(in_hospital_untested = TRUE) %>%
+      distinct(patient_id, test_date, result, in_hospital_untested),
+    by = c("patient_id", "test_date", "result")
   ) %>% 
-  ungroup()
+  filter(is.na(in_hospital_untested))
 
-## Perform matching, fill in match_id ----
-match_pos_untested = matchit(result ~ test_period,
-                             data = data_untested,
-                             exact = ~ test_period,
-                             ratio = match_ratio,
-                             replace = FALSE) %>% 
-  match.data() %>% 
-  group_by(subclass) %>% 
-  fill(match_id, .direction = "downup") %>% 
-  ungroup()
+### Log number of untested patients not in-hospital on match date ----
+data_inclusion = data_inclusion %>% 
+  left_join(data_untested %>% 
+              mutate(is_not_inhospital_untested = TRUE) %>% 
+              select(patient_id, is_not_inhospital_untested),
+            by = "patient_id")
 
-# Combine the two matches (pos-neg, pos-untested) ----
+## Filter out unnecessary columns ----
+data_untested = data_untested %>% 
+  select(patient_id, result, test_date, covid_status_tp)
+
+# Create untested data matched to positives ----
+match_untested = match_pos_neg %>% 
+  filter(covid_status_tp == "Positive") %>% 
+  pull(test_date) %>% unique() %>% sort() %>% 
+  map(function(pos_test_date){
+    
+    # Define positive and untested pools ----
+    df_pool_pos = match_pos_neg %>% 
+      filter(covid_status_tp == "Positive", test_date == pos_test_date)
+    
+    df_pool_unt = data_untested %>% 
+      filter(test_date == pos_test_date)
+    
+    # Calculate number of potential matches in each pool ----
+    n_positive = df_pool_pos %>% nrow()
+    n_untested = df_pool_unt %>% nrow()
+    n_match    = min(n_positive, floor(n_untested/match_ratio))
+    
+    # Restrict number to number of matches, assign match_id ----
+    if(n_match > 0){
+      df_out = df_pool_unt %>%
+        slice(1:(n_match*match_ratio)) %>% 
+        mutate(match_id = rep(df_pool_pos$match_id, each = match_ratio))
+    } else {
+      df_out = NULL
+    }
+    return(df_out)
+  }) %>% 
+  bind_rows()
+
+# Combine the two matches ----
 data_matched = match_pos_neg %>% 
-  bind_rows(match_pos_untested %>%
-              filter(!result == "Positive")) %>% 
-  select(match_id, patient_id, result, covid_status_tp, test_date, test_period) %>% 
+  bind_rows(match_untested) %>% 
+  select(match_id, patient_id, result, covid_status_tp, test_date) %>% 
   arrange(match_id, result) %>% 
   group_by(match_id) %>% 
   mutate(n_matches = n()) %>% 
@@ -279,7 +331,7 @@ data_inclusion = data_inclusion %>%
       ),
     by = c("patient_id")
   )
-  
+
 # Clean-up inclusion data ----
 data_inclusion = data_inclusion %>% 
   mutate(
@@ -291,6 +343,11 @@ data_inclusion = data_inclusion %>%
     alive_matched_date = case_when(
       covid_status_tp == "Untested" & is.na(alive_matched_date) ~ FALSE,
       TRUE ~ TRUE
+    ),
+    not_in_hospital_test_match_date = case_when(
+      not_in_hospital_neg_test == TRUE ~ TRUE,
+      is_not_inhospital_untested == TRUE ~ TRUE,
+      TRUE ~ FALSE
     ),
     matched = case_when(
       matched == TRUE ~ TRUE,
@@ -304,10 +361,11 @@ flowchart = data_inclusion %>%
     covid_status_tp,
     c0 = TRUE,
     c1 = c0 & not_nosocomial,
-    c2 = c1 & no_discrepant_results,
-    c3 = c2 & alive_matched_date,
-    c4 = c3 & meets_age_criteria,
-    c5 = c4 & matched
+    c2 = c1 & not_in_hospital_test_match_date,
+    c3 = c2 & no_discrepant_results,
+    c4 = c3 & alive_matched_date,
+    c5 = c4 & meets_age_criteria,
+    c6 = c5 & matched
   ) %>%
   select(-patient_id) %>%
   group_by(covid_status_tp) %>%
@@ -331,10 +389,11 @@ flowchart = data_inclusion %>%
     criteria = fct_case_when(
       crit == "c0" ~ "OpenSAFELY extract: Registered with GP, alive, with age >0 and <18 years on 01 January 2019",
       crit == "c1" ~ "-  with no probable nosocomial infection",
-      crit == "c2" ~ "-  with no same-day discrepant RT-PCR test result",
-      crit == "c3" ~ "-  is alive on test/matched date",
-      crit == "c4" ~ "-  with age between 4 and 17 years inclusive on test/matched date",
-      crit == "c5" ~ "-  successfully  matched with negative:untested:positive of 10:10:1",
+      crit == "c2" ~ "-  not hospitalised on day of matching/negative RT-PCR test result",
+      crit == "c3" ~ "-  with no same-day discrepant RT-PCR test result",
+      crit == "c4" ~ "-  is alive on test/matched date",
+      crit == "c5" ~ "-  with age between 4 and 17 years inclusive on test/matched date",
+      crit == "c6" ~ "-  successfully matched with negative:untested:positive of 5:5:1",
       TRUE ~ NA_character_
     )
   ) %>%
@@ -350,10 +409,10 @@ tbl_flowchart = flowchart %>%
   mutate(
     covid_status_tp = covid_status_tp %>% as.character(),
     covid_status_tp = if_else(row_number() == 1,covid_status_tp, "")
-    )
+  )
 
 ## Save flowchart table ----
 write_csv(tbl_flowchart, 
           here::here("output", "descriptives", "matched_cohort",
                      "tbl_flowchart.csv"))
-    
+
