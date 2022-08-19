496ca9b34364f4e414f475fdab2613607b5351b0
James Farrell <33842132+JamesAFarrell@users.noreply.github.com>
2022-08-19 12:24:49 +0100

    Update 01_generate_dummy_data.R

--- analysis/01_generate_dummy_data.R
+++ analysis/01_generate_dummy_data.R
@@ -51,52 +51,53 @@ treatment_function = c("100", "101", "102", "110", "242", "260", "280", "321",
 data_patient = data_patient %>% 
   filter(patient_id %in% sample(data_patient$patient_id, 
                                 min(n_max, nrow(.))))
+n_patient = nrow(data_patient)
 
 # Admission data ----
 dummy_data_admissions = data_patient %>% 
   transmute(
-    admission_date_1 = sample(date_range, nrow(data_patient), replace = TRUE),
-    discharge_date_1 = sample(date_range, nrow(data_patient), replace = TRUE),
-    admission_method_1 = sample(admission_method, nrow(data_patient), replace = TRUE),
-    primary_diagnosis_1 = sample(primary_diagnosis, nrow(data_patient), replace = TRUE),
-    treatment_function_1 = sample(treatment_function, nrow(data_patient), replace = TRUE),
-    critical_care_days_1 = rpois(nrow(data_patient), 0.1),
-    admission_date_2 = sample(date_range, nrow(data_patient), replace = TRUE),
-    discharge_date_2 = sample(date_range, nrow(data_patient), replace = TRUE),
-    admission_method_2 = sample(admission_method, nrow(data_patient), replace = TRUE),
-    primary_diagnosis_2 = sample(primary_diagnosis, nrow(data_patient), replace = TRUE),
-    treatment_function_2 = sample(treatment_function, nrow(data_patient), replace = TRUE),
-    critical_care_days_2 = rpois(nrow(data_patient), 0.1),
-    admission_date_3 = sample(date_range, nrow(data_patient), replace = TRUE),
-    discharge_date_3 = sample(date_range, nrow(data_patient), replace = TRUE),
-    admission_method_3 = sample(admission_method, nrow(data_patient), replace = TRUE),
-    primary_diagnosis_3 = sample(primary_diagnosis, nrow(data_patient), replace = TRUE),
-    treatment_function_3 = sample(treatment_function, nrow(data_patient), replace = TRUE),
-    critical_care_days_3 = rpois(nrow(data_patient), 0.1),
-    admission_date_4 = sample(date_range, nrow(data_patient), replace = TRUE),
-    discharge_date_4 = sample(date_range, nrow(data_patient), replace = TRUE),
-    admission_method_4 = sample(admission_method, nrow(data_patient), replace = TRUE),
-    primary_diagnosis_4 = sample(primary_diagnosis, nrow(data_patient), replace = TRUE),
-    treatment_function_4 = sample(treatment_function, nrow(data_patient), replace = TRUE),
-    critical_care_days_4 = rpois(nrow(data_patient), 0.1),
-    admission_date_5 = sample(date_range, nrow(data_patient), replace = TRUE),
-    discharge_date_5 = sample(date_range, nrow(data_patient), replace = TRUE),
-    admission_method_5 = sample(admission_method, nrow(data_patient), replace = TRUE),
-    primary_diagnosis_5 = sample(primary_diagnosis, nrow(data_patient), replace = TRUE),
-    treatment_function_5 = sample(treatment_function, nrow(data_patient), replace = TRUE),
-    critical_care_days_5 = rpois(nrow(data_patient), 0.1),
-    admission_date_6 = sample(date_range, nrow(data_patient), replace = TRUE),
-    discharge_date_6 = sample(date_range, nrow(data_patient), replace = TRUE),
-    admission_method_6 = sample(admission_method, nrow(data_patient), replace = TRUE),
-    primary_diagnosis_6 = sample(primary_diagnosis, nrow(data_patient), replace = TRUE),
-    treatment_function_6 = sample(treatment_function, nrow(data_patient), replace = TRUE),
-    critical_care_days_6 = rpois(nrow(data_patient), 0.1),
-    admission_date_7 = sample(date_range, nrow(data_patient), replace = TRUE),
-    discharge_date_7 = sample(date_range, nrow(data_patient), replace = TRUE),
-    admission_method_7 = sample(admission_method, nrow(data_patient), replace = TRUE),
-    primary_diagnosis_7 = sample(primary_diagnosis, nrow(data_patient), replace = TRUE),
-    treatment_function_7 = sample(treatment_function, nrow(data_patient), replace = TRUE),
-    critical_care_days_7 = rpois(nrow(data_patient), 0.1),
+    admission_date_1 = sample(date_range, n_patient, replace = TRUE),
+    discharge_date_1 = sample(date_range, n_patient, replace = TRUE),
+    admission_method_1 = sample(admission_method, n_patient, replace = TRUE),
+    primary_diagnosis_1 = sample(primary_diagnosis, n_patient, replace = TRUE),
+    treatment_function_1 = sample(treatment_function, n_patient, replace = TRUE),
+    critical_care_days_1 = rpois(n_patient, 0.1),
+    admission_date_2 = sample(date_range, n_patient, replace = TRUE),
+    discharge_date_2 = sample(date_range, n_patient, replace = TRUE),
+    admission_method_2 = sample(admission_method, n_patient, replace = TRUE),
+    primary_diagnosis_2 = sample(primary_diagnosis, n_patient, replace = TRUE),
+    treatment_function_2 = sample(treatment_function, n_patient, replace = TRUE),
+    critical_care_days_2 = rpois(n_patient, 0.1),
+    admission_date_3 = sample(date_range, n_patient, replace = TRUE),
+    discharge_date_3 = sample(date_range, n_patient, replace = TRUE),
+    admission_method_3 = sample(admission_method, n_patient, replace = TRUE),
+    primary_diagnosis_3 = sample(primary_diagnosis, n_patient, replace = TRUE),
+    treatment_function_3 = sample(treatment_function, n_patient, replace = TRUE),
+    critical_care_days_3 = rpois(n_patient, 0.1),
+    admission_date_4 = sample(date_range, n_patient, replace = TRUE),
+    discharge_date_4 = sample(date_range, n_patient, replace = TRUE),
+    admission_method_4 = sample(admission_method, n_patient, replace = TRUE),
+    primary_diagnosis_4 = sample(primary_diagnosis, n_patient, replace = TRUE),
+    treatment_function_4 = sample(treatment_function, n_patient, replace = TRUE),
+    critical_care_days_4 = rpois(n_patient, 0.1),
+    admission_date_5 = sample(date_range, n_patient, replace = TRUE),
+    discharge_date_5 = sample(date_range, n_patient, replace = TRUE),
+    admission_method_5 = sample(admission_method, n_patient, replace = TRUE),
+    primary_diagnosis_5 = sample(primary_diagnosis, n_patient, replace = TRUE),
+    treatment_function_5 = sample(treatment_function, n_patient, replace = TRUE),
+    critical_care_days_5 = rpois(n_patient, 0.1),
+    admission_date_6 = sample(date_range, n_patient, replace = TRUE),
+    discharge_date_6 = sample(date_range, n_patient, replace = TRUE),
+    admission_method_6 = sample(admission_method, n_patient, replace = TRUE),
+    primary_diagnosis_6 = sample(primary_diagnosis, n_patient, replace = TRUE),
+    treatment_function_6 = sample(treatment_function, n_patient, replace = TRUE),
+    critical_care_days_6 = rpois(n_patient, 0.1),
+    admission_date_7 = sample(date_range, n_patient, replace = TRUE),
+    discharge_date_7 = sample(date_range, n_patient, replace = TRUE),
+    admission_method_7 = sample(admission_method, n_patient, replace = TRUE),
+    primary_diagnosis_7 = sample(primary_diagnosis, n_patient, replace = TRUE),
+    treatment_function_7 = sample(treatment_function, n_patient, replace = TRUE),
+    critical_care_days_7 = rpois(n_patient, 0.1),
     patient_id = patient_id,
   )
 
@@ -149,15 +150,16 @@ dummy_data_admissions = dummy_data_admissions %>%
   relocate(patient_id, .after = last_col())
 
 # Outpatient data ----
+lambda_outpatient = 0.15
 dummy_data_outpatient = data_patient %>% 
   transmute(
-    outpatient_count_1 = rpois(nrow(data_patient), lambda = 0.25),
-    outpatient_count_2 = rpois(nrow(data_patient), lambda = 0.25),
-    outpatient_count_3 = rpois(nrow(data_patient), lambda = 0.25),
-    outpatient_count_4 = rpois(nrow(data_patient), lambda = 0.25),
-    outpatient_count_5 = rpois(nrow(data_patient), lambda = 0.25),
-    outpatient_count_6 = rpois(nrow(data_patient), lambda = 0.25),
-    outpatient_count_7 = rpois(nrow(data_patient), lambda = 0.25),
+    outpatient_count_1 = rpois(n_patient, lambda = lambda_outpatient),
+    outpatient_count_2 = rpois(n_patient, lambda = lambda_outpatient),
+    outpatient_count_3 = rpois(n_patient, lambda = lambda_outpatient),
+    outpatient_count_4 = rpois(n_patient, lambda = lambda_outpatient),
+    outpatient_count_5 = rpois(n_patient, lambda = lambda_outpatient),
+    outpatient_count_6 = rpois(n_patient, lambda = lambda_outpatient),
+    outpatient_count_7 = rpois(n_patient, lambda = lambda_outpatient),
     outpatient_count_week = outpatient_count_1 + outpatient_count_2 +
       outpatient_count_3 + outpatient_count_4 + outpatient_count_5 +
       outpatient_count_6 + outpatient_count_7,
@@ -165,111 +167,54 @@ dummy_data_outpatient = data_patient %>%
   )
 
 # GP data ----
+lambda_gp = 0.25
 dummy_data_gp = data_patient %>% 
   transmute(
-    gp_count_1 = rpois(nrow(data_patient), lambda = 0.25),
-    gp_count_2 = rpois(nrow(data_patient), lambda = 0.25),
-    gp_count_3 = rpois(nrow(data_patient), lambda = 0.25),
-    gp_count_4 = rpois(nrow(data_patient), lambda = 0.25),
-    gp_count_5 = rpois(nrow(data_patient), lambda = 0.25),
-    gp_count_6 = rpois(nrow(data_patient), lambda = 0.25),
-    gp_count_7 = rpois(nrow(data_patient), lambda = 0.25),
+    gp_count_1 = rpois(n_patient, lambda = lambda_gp),
+    gp_count_2 = rpois(n_patient, lambda = lambda_gp),
+    gp_count_3 = rpois(n_patient, lambda = lambda_gp),
+    gp_count_4 = rpois(n_patient, lambda = lambda_gp),
+    gp_count_5 = rpois(n_patient, lambda = lambda_gp),
+    gp_count_6 = rpois(n_patient, lambda = lambda_gp),
+    gp_count_7 = rpois(n_patient, lambda = lambda_gp),
     gp_count_week = gp_count_1 + gp_count_2 + gp_count_3 + gp_count_4 + 
       gp_count_5 + gp_count_6 + gp_count_7,
     patient_id = patient_id,
   )
 
 # Negative covid testing data ----
+lambda_covid_negative = 0.15
 dummy_data_testing_negative = data_patient %>% 
   transmute(
-    covid_negative_test_date_1 = sample(date_range_testing, nrow(data_patient), replace = TRUE),
-    covid_negative_test_date_2 = sample(date_range_testing, nrow(data_patient), replace = TRUE),
-    covid_negative_test_date_3 = sample(date_range_testing, nrow(data_patient), replace = TRUE),
-    #covid_negative_test_date_4 = sample(date_range_testing, nrow(data_patient), replace = TRUE),
+    covid_negative_test_count_1 = rpois(n_patient, lambda = lambda_covid_negative),
+    covid_negative_test_count_2 = rpois(n_patient, lambda = lambda_covid_negative),
+    covid_negative_test_count_3 = rpois(n_patient, lambda = lambda_covid_negative),
+    covid_negative_test_count_4 = rpois(n_patient, lambda = lambda_covid_negative),
+    covid_negative_test_count_5 = rpois(n_patient, lambda = lambda_covid_negative),
+    covid_negative_test_count_6 = rpois(n_patient, lambda = lambda_covid_negative),
+    covid_negative_test_count_7 = rpois(n_patient, lambda = lambda_covid_negative),
+    covid_negative_test_week_count = covid_negative_test_count_1 + covid_negative_test_count_2 +
+      covid_negative_test_count_3 + covid_negative_test_count_4 + covid_negative_test_count_5 +
+      covid_negative_test_count_6 + covid_negative_test_count_7,
     patient_id = patient_id,
   )
-
-## Make dates consecutive, create missing values based on incidence ----
-dummy_data_testing_negative = dummy_data_testing_negative %>%
-  mutate_at(vars(starts_with(c("covid_negative_test_date"))),
-            as.character) %>%
-  pivot_longer(-patient_id,
-               names_to = c("variable", "index"),
-               names_pattern = "^(.*)_(\\d+)") %>% 
-  mutate(value = value %>% ymd()) %>% 
-  group_by(patient_id) %>%
-  mutate(value = sort(value) %>% as.character()) %>%
-  ungroup() %>% 
-  mutate(prob = rep(runif(n()))) %>% 
-  filter(prob <= 0.3) %>% 
-  group_by(patient_id) %>% 
-  mutate(index = rep(1:n()),
-         var_name = paste(variable, index, sep = "_")) %>% 
-  ungroup() %>% 
-  arrange(index, variable) %>% 
-  select(-c(prob, variable, index))
-
-## Ensure valid number of rows and columns ----
-dummy_data_testing_negative = tibble(
-  patient_id = unique(data_patient$patient_id) %>% rep(each = n_negative_test),
-  var_name = rep(paste0(rep("covid_negative_test_date_", n_negative_test), c(1:n_negative_test)),
-                 length(unique(data_patient$patient_id)))
-) %>% 
-  left_join(dummy_data_testing_negative)%>% 
-  pivot_wider(names_from = var_name, values_from = value) 
-
-## Add covid_negative_test_count, relocate patient_id to last row ----
-dummy_data_testing_negative = dummy_data_testing_negative %>% 
-  mutate(covid_negative_test_count = dummy_data_testing_negative %>%
-           transmute(across(starts_with("covid_negative_test_date"), ~ !is.na(.x))) %>%
-           mutate(covid_negative_test_count = reduce(select(., starts_with("covid_negative_test_date")), `+`)) %>% 
-           pull(covid_negative_test_count)) %>% 
-  relocate(patient_id, .after = last_col())
 
 # Positive covid testing data ----
+lambda_covid_positive = 0.01
 dummy_data_testing_positive = data_patient %>% 
   transmute(
-    covid_positive_test_date_1 = sample(date_range_testing, nrow(data_patient), replace = TRUE),
-    covid_positive_test_date_2 = sample(date_range_testing, nrow(data_patient), replace = TRUE),
+    covid_positive_test_count_1 = rpois(n_patient, lambda = lambda_covid_positive),
+    covid_positive_test_count_2 = rpois(n_patient, lambda = lambda_covid_positive),
+    covid_positive_test_count_3 = rpois(n_patient, lambda = lambda_covid_positive),
+    covid_positive_test_count_4 = rpois(n_patient, lambda = lambda_covid_positive),
+    covid_positive_test_count_5 = rpois(n_patient, lambda = lambda_covid_positive),
+    covid_positive_test_count_6 = rpois(n_patient, lambda = lambda_covid_positive),
+    covid_positive_test_count_7 = rpois(n_patient, lambda = lambda_covid_positive),
+    covid_positive_test_week_count = covid_positive_test_count_1 + covid_positive_test_count_2 +
+      covid_positive_test_count_3 + covid_positive_test_count_4 + covid_positive_test_count_5 +
+      covid_positive_test_count_6 + covid_positive_test_count_7,
     patient_id = patient_id,
   )
-
-## Make dates consecutive, create missing values based on incidence ----
-dummy_data_testing_positive = dummy_data_testing_positive %>%
-  mutate_at(vars(starts_with(c("covid_positive_test_date"))),
-            as.character) %>%
-  pivot_longer(-patient_id,
-               names_to = c("variable", "index"),
-               names_pattern = "^(.*)_(\\d+)") %>% 
-  mutate(value = value %>% ymd()) %>% 
-  group_by(patient_id) %>%
-  mutate(value = sort(value) %>% as.character()) %>%
-  ungroup() %>% 
-  mutate(prob = rep(runif(n()))) %>% 
-  filter(prob <= 0.05) %>% 
-  group_by(patient_id) %>% 
-  mutate(index = rep(1:n()),
-         var_name = paste(variable, index, sep = "_")) %>% 
-  ungroup() %>% 
-  arrange(index, variable) %>% 
-  select(-c(prob, variable, index))
-
-## Ensure valid number of rows and columns ----
-dummy_data_testing_positive = tibble(
-  patient_id = unique(data_patient$patient_id) %>% rep(each = n_positive_test),
-  var_name = rep(paste0(rep("covid_positive_test_date_", n_positive_test), c(1:n_positive_test)),
-                 length(unique(data_patient$patient_id)))
-) %>% 
-  left_join(dummy_data_testing_positive)%>% 
-  pivot_wider(names_from = var_name, values_from = value) 
-
-## Add covid_positive_test_count, relocate patient_id to last row ----
-dummy_data_testing_positive = dummy_data_testing_positive %>% 
-  mutate(covid_positive_test_count = dummy_data_testing_positive %>%
-           transmute(across(starts_with("covid_positive_test_date"), ~ !is.na(.x))) %>%
-           mutate(covid_positive_test_count = reduce(select(., starts_with("covid_positive_test_date")), `+`)) %>% 
-           pull(covid_positive_test_count)) %>% 
-  relocate(patient_id, .after = last_col())
   
 # Save dummy data ----
 write_csv(dummy_data_admissions,       gzfile(here::here("output", "dummy_data", "dummy_data_admissions.csv.gz")))
