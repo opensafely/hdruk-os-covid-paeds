5598677b4c6ce0572fd535ac911a2b1a710f1553
James Farrell <33842132+JamesAFarrell@users.noreply.github.com>
2022-09-09 14:23:07 +0100

    Update 07_ipw.R

--- analysis/07_ipw.R
+++ analysis/07_ipw.R
@@ -10,6 +10,7 @@
 
 # Load packages ----
 library(tidyverse)
+library(lubridate)
 library(WeightIt)
 library(cobalt)
 
@@ -17,28 +18,218 @@ library(cobalt)
 source(here::here("analysis", "00_utility_functions.R"))
 source(here::here("analysis", "00_lookup_tables.R"))
 
+# Output directories ----
+dir.create(here::here("output", "data"),
+           showWarnings = FALSE, recursive=TRUE)
+dir.create(here::here("output", "descriptives", "matched_cohort", "ipw"),
+           showWarnings = FALSE, recursive=TRUE)
+dir.create(here::here("output", "descriptives", "matched_cohort", "ipw", "balance_plot"),
+           showWarnings = FALSE, recursive=TRUE)
+
+
 # Load datasets ----
 data_matched    = read_rds(here::here("output", "data", "data_matched.rds"))
 data_patient    = read_rds(here::here("output", "data", "data_patient.rds"))
+data_testing    = read_rds(here::here("output", "data", "data_testing.rds"))
+data_admissions = read_rds(here::here("output", "data", "data_admissions.rds"))
+data_outpatient = read_rds(here::here("output", "data", "data_outpatient.rds"))
+data_gp         = read_rds(here::here("output", "data", "data_gp.rds"))
 
+# Calculate weighting variables ----
+## Calculate time-dependent variables on matched date ----
 data_matched = data_matched %>%
   left_join(data_patient, by = c("patient_id", "covid_status_tp")) %>% 
   calc_indexed_variables(data_matched %>% pull(test_date)) 
 
-data_matched %>% 
-  select(sex, age_group, cerebral_palsy) %>%
-  droplevels() %>% 
-  summary()
+## Ethnicity - explicitly code missing as factor
+data_matched = data_matched %>% 
+  mutate(ethnicity = ethnicity %>% 
+           fct_explicit_na())
 
+## Calculate number of covid tests days in prior year to match date ----
+data_matched = data_matched %>% 
+  left_join(
+    data_testing %>% 
+      left_join(data_matched %>% 
+                  select(patient_id, match_date = test_date),
+                by = "patient_id") %>% 
+      filter(test_date >= match_date - years(1),
+             test_date < match_date) %>% 
+      distinct(patient_id, test_date) %>% 
+      count(patient_id) %>% 
+      rename(n_covid_tests = n),
+    by = c("patient_id")
+  ) %>% 
+  replace_na(list(n_covid_tests = 0))
 
+## Calculate number of beddays in prior year to match date ----
+data_matched = data_matched %>% 
+  left_join(
+    data_admissions %>% 
+      left_join(data_matched %>% 
+                  select(patient_id, test_date),
+                by = "patient_id") %>% 
+      filter(admission_date >= test_date - years(1),
+             admission_date < test_date) %>% 
+      mutate(
+        n_beddays = case_when(
+          admission_date == discharge_date ~ 0.5,
+          TRUE ~ (pmin(discharge_date, test_date - days(1)) - admission_date) %>% 
+            as.numeric()
+        )
+      ) %>% 
+      select(patient_id, n_beddays),
+    by = "patient_id"
+  ) %>% 
+  replace_na(list(n_beddays = 0))
 
-#Using WeightIt to generate weights with multinomial logistic regression
-W_out = weightit(covid_status_tp ~ sex + age_group + cerebral_palsy,
+## Calculate number of outpatient appointments in prior year to match date ----
+data_matched = data_matched %>% 
+  left_join(
+    data_outpatient %>%
+      filter(is.na(specialty)) %>% 
+      left_join(
+        data_matched %>% 
+          select(patient_id, test_date),
+        by = "patient_id"
+      ) %>%
+      filter(outpatient_date >= test_date - years(1),
+             outpatient_date <  test_date) %>% 
+      group_by(patient_id) %>% 
+      summarise(
+        n_outpatient = sum(outpatient_count)
+      ),
+    by = "patient_id"
+  ) %>% 
+  replace_na(list(n_outpatient = 0))
+    
+## Calculate number of GP contact days in prior year to match date ----
+data_matched = data_matched %>% 
+  left_join(
+    data_gp %>%
+      filter(str_starts(code_type, "KM_") |
+               str_starts(code_type, "mapped_1") |
+               str_starts(code_type, "mapped_2")) %>% 
+      left_join(
+        data_matched %>% 
+          select(patient_id, test_date),
+        by = "patient_id"
+      ) %>%
+      filter(gp_date >= test_date - years(1),
+             gp_date <  test_date) %>%
+      distinct(patient_id, gp_date) %>%
+      count(patient_id) %>% 
+      rename(n_gp = n),
+    by = "patient_id"
+  ) %>% 
+  replace_na(list(n_gp = 0))
+
+# Calculate weighting ----
+## Predictors
+weight_variables = c(
+  # Demographics
+  "age_group", "sex", "ethnicity", "imd_Q5_2019",
+  "region_2019", "rural_urban_2019",
+  
+  # Comorbidities
+  "comorbidity_count.factor", 
+  "asthma", "cancer", "cerebral_palsy", "chronic_infections", "cystic_fibrosis",
+  "devices_and_stomas", "diabetes", "endocrine_disorders", 
+  "epilepsy", "gastrointestinal_disorders", "haematological_disorders",
+  "immunological_disorders", "learning_and_behaviour_difficulties",
+  "mental_illness", "musculoskeletal_and_rheum",
+  "severe_mental_illness", "transplant",
+  
+  # Resource use and covid testing
+  "n_covid_tests",
+  "n_beddays", "n_outpatient", "n_gp"
+)
+
+## Model forumla ----
+model_formula = paste0("covid_status_tp ~ ",
+                       paste(weight_variables, collapse = " + ")) %>% 
+  as.formula()
+
+## Using WeightIt to generate weights with multinomial logistic regression ----
+data_weights = weightit(model_formula,
                  data = data_matched,
                  method = "ps",
                  use.mlogit = FALSE)
 
+write_rds(x = data_weights,
+          file = here::here("output", "data", "data_weights.rds"),
+          compress="gz")
 
+# Assess balance across groups ----
+## Add weights to matched data ----
+data_matched = data_matched %>% 
+  mutate(weights = data_weights$weights)
+
+## Summary of unweighted ----
+summary_unweighted = data_matched %>% 
+  summary_factorlist(dependent = "covid_status_tp",
+                     explanatory = weight_variables)
+
+write_csv(x = summary_unweighted,
+          file = here::here("output", "descriptives", "matched_cohort", "ipw", 
+                            "summary_unweighted.csv"))
+
+## Summary of weighted ----
+summary_weighted = data_matched %>% 
+  summary_factorlist(dependent = "covid_status_tp",
+                     explanatory = weight_variables,
+                     weights = "weights")
+
+write_csv(x = summary_weighted,
+          file = here::here("output", "descriptives", "matched_cohort", "ipw",
+                            "summary_weighted.csv"))
+
+## Effective sample size ----
+balance_summary = bal.tab(data_weights, un = TRUE)
+
+table_effective_sample_size = balance_summary$Observations %>% 
+  as_tibble(rownames = "Adjustment")
+
+write_csv(x = table_effective_sample_size,
+          file = here::here("output", "descriptives", "matched_cohort", "ipw",
+                            "table_effective_sample_size.csv"))
+
+## Assess balance for each pair of treatments ----
+pair_balance = bal.tab(data_weights, un = TRUE, disp.means = TRUE, which.treat = .all)
+
+table_pair_balance = map2(pair_balance$Pair.Balance,
+                          names(pair_balance$Pair.Balance),
+                          function(pair_balance_list, list_name){
+  output = pair_balance_list$Balance %>%
+    as_tibble(rownames = "var") %>% 
+    mutate(pair = list_name)
+}) %>% 
+  bind_rows()
+
+write_csv(x = table_pair_balance,
+          file = here::here("output", "descriptives", "matched_cohort", "ipw",
+                            "table_pair_balance.csv"))
+
+## Assess balance graphically ----
+weight_variables %>% 
+  map(function(weight_var){
+    balance_plot = bal.plot(data_weights, weight_var, which = "both")
+    ggsave(
+      filename = paste0("balance_plot_", weight_var, ".jpeg"),
+      plot = balance_plot,
+      path = here::here("output", "descriptives", "matched_cohort", "ipw",
+                        "balance_plot"),
+      width = 10, height = 7, units = "in")
+  })
+
+# Create love plot ----
+love_plot = love.plot(data_weights, thresholds = c(m = .1), binary = "std",
+                      which.treat = .all, abs = TRUE, position = "bottom")
+
+ggsave(filename = paste0("love_plot.jpeg"),
+       plot = love_plot,
+       path = here::here("output", "descriptives", "matched_cohort", "ipw"),
+       width = 10, height = 10, units = "in")
 
 
 
