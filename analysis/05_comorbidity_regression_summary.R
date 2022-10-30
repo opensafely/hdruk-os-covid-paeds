a7fc978556548b275d467798ef948fbb162acb2d
James Farrell <33842132+JamesAFarrell@users.noreply.github.com>
2022-10-30 17:39:31 +0000

    Update 05_comorbidity_regression_summary.R

--- analysis/05_comorbidity_regression_summary.R
+++ analysis/05_comorbidity_regression_summary.R
@@ -11,7 +11,7 @@ theme_set(theme_bw())
 # Command arguments ----
 args = commandArgs(trailingOnly = TRUE)
 if(length(args) == 0){
-  model_type     = "poisson"
+  model_type     = "negative_binomial"
 } else{
   model_type     = args[[1]]
 }
@@ -25,79 +25,6 @@ if(!model_type %in% c("poisson", "negative_binomial")){
 # Create output directory ----
 dir.create(here::here("output", "comorbidity_multivar", model_type, "summary"),
            showWarnings = FALSE, recursive=TRUE)
-
-
-c("gp", "outpatient", "admissions", "beddays") %>% 
-  walk(function(resource_type){
-    
-    model_fit = read_rds(here::here("output", "comorbidity_multivar", model_type,
-                                    resource_type, "model", "model_fit.rds"))
-    
-    tbl_model_coef = model_fit %>%
-      tidy() %>% 
-      tidy_add_term_labels(model = model_fit) %>% 
-      left_join(
-        model_fit %>% 
-          confint.default() %>% 
-          as_tibble(rownames = "term") %>% 
-          rename(ci_lower = "2.5 %", ci_upper = "97.5 %"),
-        by = "term"
-      )
-    
-    write_csv(tbl_model_coef, 
-              here::here("output", "comorbidity_multivar", model_type,
-                         resource_type, "tbl_model_coef.csv"))
-    
-    # Read model coefficients
-    tbl_model_coef = read_csv(here::here("output", "comorbidity_multivar", model_type,
-                                         resource_type, "tbl_model_coef.csv"))
-    # Tidy up table
-    tbl_model_coef_tidy = tbl_model_coef %>% 
-      filter(!term == "(Intercept)") %>% 
-      mutate(
-        year = str_extract(term, "(?<=year)\\d{4}"),
-        var_lab = paste0(str_remove(var_label, "Year \\* "), ": ",
-                         str_remove(label, "\\d{4} \\* ")) %>% 
-          str_remove(": \\d{4}"),
-        var_lab = var_lab %>% 
-          factor(levels = unique(var_lab)) %>% 
-          fct_rev(),
-        var_type = case_when(
-          var_type == "interaction" ~ "Interaction",
-          str_detect(term, "_with_") ~ "Interaction",
-          variable == "year" ~ "Year effect",
-          TRUE ~ "Baseline effect"
-        ) %>% 
-          factor() %>% 
-          fct_relevel("Year effect", "Baseline effect")
-      ) %>% 
-      replace_na(list(year = "2019"))
-    
-    
-    plot_model_coef = tbl_model_coef_tidy %>% 
-      ggplot(aes(x = estimate, xmin = ci_lower, xmax = ci_upper,
-                 y = var_lab, colour = var_type)) +
-      geom_point() +
-      geom_errorbar(width = 0.25) +
-      geom_vline(xintercept = 0, linetype = "dashed") +
-      facet_wrap(~ year, nrow = 1) + 
-      labs(
-        y = NULL, x = "Coefficient (95% confidence interval)",
-        colour = "Variable type"
-      ) +
-      theme(
-        legend.position = "bottom"
-      )
-    
-    # Save IRR plot ----
-    ggsave(filename = "plot_model_coef.jpeg",
-           plot = plot_model_coef,
-           path = here::here("output", "comorbidity_multivar", model_type,
-                             resource_type),
-           width = 12, height = 8, units = "in")
-    
-  }
-  )
 
 # Import IRRs for each resource and comorbidity ----
 tbl_irr_combined = c("gp", "outpatient", "admissions", "beddays") %>% 
