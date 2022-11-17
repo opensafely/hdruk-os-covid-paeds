1b1407264cc63a13824b86e9a5e7348cbf1d0508
James Farrell <33842132+JamesAFarrell@users.noreply.github.com>
2022-11-17 00:19:12 +0000

    Update 12_2_LCMM_model.R

--- analysis/12_2_LCMM_model.R
+++ analysis/12_2_LCMM_model.R
@@ -36,7 +36,7 @@ data_resource_lcmm = as.data.frame(data_resource_lcmm)
 
 # Run LCMM model ----
 ## Set model parameters ----
-max_iter = 2000 # Maximum number of iterations
+max_iter = 50000 # Maximum number of iterations
 
 ## Run lcmm ----
 if (ng == 1){
@@ -51,7 +51,7 @@ if (ng == 1){
                     nproc = 4)
   
   # lcmm_model = lcmm(fixed = hospital_use ~ 1 + indexed_month + I(indexed_month^2) + I(indexed_month^3),
-  #                   random = ~1 + period,
+  #                   random = ~1 + indexed_month,
   #                   subject = "patient_id",
   #                   ng = ng,
   #                   maxiter = max_iter,
@@ -69,7 +69,7 @@ if (ng == 1){
   lcmm_model = hlme(fixed = hospital_use ~ bSpline(indexed_month, degree = 3, knots = 7),
                     random= ~bSpline(indexed_month, degree = 3, knots = 7),
                     mixture = ~bSpline(indexed_month, degree = 3, knots = 7),
-                    #classmb = ~1, 
+                    #classmb = ~1,
                     ng = ng,
                     B = lcmm_model_1,
                     data = data_resource_lcmm,
