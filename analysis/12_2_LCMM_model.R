9b2fa7e5e4ece2f02a633103fac142c06dffa4b6
James Farrell <33842132+JamesAFarrell@users.noreply.github.com>
2022-11-30 15:26:11 +0000

    try bSpline(degree = 1, knots = 7)

--- analysis/12_2_LCMM_model.R
+++ analysis/12_2_LCMM_model.R
@@ -59,8 +59,8 @@ max_iter = 5000 # Maximum number of iterations
 ## Run lcmm ----
 if (ng == 1){
 
-  lcmm_model = hlme(fixed = resource_use ~ poly(followup_month, degree = 2, raw = TRUE), # + followup_month, #bSpline(followup_month, degree = 3, knots = 7),
-                    random = ~ poly(followup_month, degree = 2, raw = TRUE), #~bSpline(followup_month, degree = 3, knots = 7),
+  lcmm_model = hlme(fixed = resource_use ~ bSpline(followup_month, degree = 1, knots = 7),
+                    random = ~ bSpline(followup_month, degree = 1, knots = 7),
                     subject = "patient_id",
                     ng = ng,
                     maxiter = max_iter,
@@ -85,9 +85,9 @@ if (ng == 1){
     here::here("output", "lcmm", resource_type, "models", "lcmm_model_1.rds"))
   
   # Run hlme ----
-  lcmm_model = hlme(fixed = resource_use ~ poly(followup_month, degree = 2, raw = TRUE), #bSpline(followup_month, degree = 3, knots = 7),
-                    random = ~ poly(followup_month, degree = 2, raw = TRUE), #~bSpline(followup_month, degree = 3, knots = 7),
-                    mixture = ~ poly(followup_month, degree = 2, raw = TRUE), #~bSpline(followup_month, degree = 3, knots = 7),
+  lcmm_model = hlme(fixed = resource_use ~ bSpline(followup_month, degree = 1, knots = 7),
+                    random = ~ bSpline(followup_month, degree = 1, knots = 7),
+                    mixture = ~ bSpline(followup_month, degree = 1, knots = 7),
                     classmb = ~1,
                     ng = ng,
                     B = lcmm_model_1,
