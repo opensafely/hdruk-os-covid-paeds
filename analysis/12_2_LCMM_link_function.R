4eea94d9ebe73f4d4dae25665b568734f9e7a413
James Farrell <33842132+JamesAFarrell@users.noreply.github.com>
2022-12-07 13:09:50 +0000

    Update 12_2_LCMM_link_function.R

--- analysis/12_2_LCMM_link_function.R
+++ analysis/12_2_LCMM_link_function.R
@@ -56,15 +56,14 @@ data_resource_lcmm = as.data.frame(data_resource_lcmm)
 
 # Run LCMM model ----
 ## Set model parameters ----
-max_iter = 1000 # Maximum number of iterations
+max_iter = 2000 # Maximum number of iterations
 
 
 # Create table of link functions to model ----
 link_function = tribble(
   ~label,                           ~link_function,      ~interior_nodes,
   "linear",                         "linear",            NULL,
-  "splines (7 equi-distant)",       "7-equi-splines",    NULL,
-  "splines (3 manual - knot: 2)",   "3-manual-splines",  c(2),
+  "splines (5 equi-distant)",       "5-equi-splines",    NULL,
   "splines (3 manual - knot: 3)",   "3-manual-splines",  c(3),
   "splines (3 manual - knot: 5)",   "3-manual-splines",  c(5)
 )
