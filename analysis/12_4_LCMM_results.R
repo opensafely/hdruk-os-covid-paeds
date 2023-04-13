df1d650f7b748b4c351590a8d2f27bfa0c1e4786
James Farrell <33842132+JamesAFarrell@users.noreply.github.com>
2023-04-13 21:24:19 +0100

    Update 12_4_LCMM_results.R

--- analysis/12_4_LCMM_results.R
+++ analysis/12_4_LCMM_results.R
@@ -179,10 +179,17 @@ tbl_obs_trajectory = data_resource %>%
   unnest_wider(resource_use) %>% 
   ungroup()
 
-## Save observed trajectory table ----
-write_csv(tbl_obs_trajectory,
-          here::here("output", "lcmm", resource_type, "obs_trajectory",
-                     paste0("tbl_obs_trajectory_", ng, ".csv")))
+## Apply disclosure controls ----
+tbl_obs_trajectory = tbl_obs_trajectory %>% 
+  mutate(
+    n_patient = if_else(n_patient <= count_redact, NA_real_,
+                        n_patient %>% plyr::round_any(count_round)),
+    n_events = if_else(n_patient <= count_redact, NA_real_, n_events),
+    Mean = if_else(n_patient <= count_redact, NA_real_, n_events / n_patient),
+    Lower = if_else(n_patient <= count_redact, NA_real_, Lower),
+    Upper = if_else(n_patient <= count_redact, NA_real_, Upper),
+  )
+
 
 ## Plot observed trajectory ----
 y_label = case_when(
@@ -211,6 +218,18 @@ ggsave(filename = here::here("output", "lcmm", resource_type, "obs_trajectory",
        plot = plot_obs_trajectory,
        height = 7, width = 10, units = "in")
 
+## Save observed trajectory table ----
+tbl_obs_trajectory = tbl_obs_trajectory %>% 
+  mutate(across(c(n_patient, n_events, Mean, Lower, Upper), as.character)) %>% 
+  replace_na(list(n_patient = "[REDACTED]",
+                  n_events = "[REDACTED]",
+                  Mean = "[REDACTED]",
+                  Lower = "[REDACTED]",
+                  Upper = "[REDACTED]"))
+
+write_csv(tbl_obs_trajectory,
+          here::here("output", "lcmm", resource_type, "obs_trajectory",
+                     paste0("tbl_obs_trajectory_", ng, ".csv")))
 
 
 # Save model summary ----
